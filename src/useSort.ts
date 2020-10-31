505b938b094b1a4451bdbdfc70f64933173a7f29
Andy D. Ng <itc.anhduy@gmail.com>
2020-10-31 22:19:30 +0700

    [ANDY] Add description

--- src/useSort.ts
+++ src/useSort.ts
@@ -3,6 +3,35 @@ import { useState, useEffect } from 'react'
 import { isString, head, merge, orderBy } from 'lodash'
 import { ISort, IUseSort, TDirection, TSortValues } from 'types/sort'
 
+/**
+ * @param data Array of data
+ * @param initSort Default sort column
+ * @param direction Default direction 'asc' | 'desc'
+ * @param onSortBy
+ * Default sort function
+ * orders of the iteratees to sort by. If `orders` is unspecified, all values
+ * are sorted in ascending order. Otherwise, specify an order of "desc" for
+ * descending or "asc" for ascending sort order of corresponding values.
+ *
+ * @category Collection
+ * @param collection The collection to iterate over.
+ * @param [iteratees=[_.identity]] The iteratees to sort by.
+ * @param [orders] The sort orders of `iteratees`.
+ * @param- {Object} [guard] Enables use as an iteratee for functions like `_.reduce`.
+ * @returns Returns the new sorted array.
+ * @example
+ *
+ * var users = [
+ *   { 'user': 'fred',   'age': 48 },
+ *   { 'user': 'barney', 'age': 34 },
+ *   { 'user': 'fred',   'age': 42 },
+ *   { 'user': 'barney', 'age': 36 }
+ * ];
+ *
+ * // sort by `user` in ascending order and by `age` in descending order
+ * _.orderBy(users, ['user', 'age'], ['asc', 'desc']);
+ * // => objects for [['barney', 36], ['barney', 34], ['fred', 48], ['fred', 42]]
+ */
 const useSort = <T>(
   data: Array<T>,
   initSort: keyof T,
